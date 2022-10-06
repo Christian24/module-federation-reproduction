f34e50fd229540472da1366b00153692fd35b407
Christian Siebmanns <christian.siebmanns@hotmail.de>
2022-10-06 23:04:43 +0200

    Simplify configuration

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -1,51 +1,51 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ModuleFederationPlugin } = require('webpack').container;
-const ExternalTemplateRemotesPlugin = require('external-remotes-plugin');
-const path = require('path');
-const app2Config = require('../app2/webpack.config.js');
-const app3Config = require('../app3/webpack.config.js');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
+const path = require("path");
+const app2Config = require("../app2/webpack.config.js");
+const app3Config = require("../app3/webpack.config.js");
 
 module.exports = [
   {
-    entry: './src/index',
-    mode: 'development',
+    entry: "./src/index",
+    mode: "development",
     devServer: {
-      static: path.join(__dirname, 'dist'),
+      static: path.join(__dirname, "dist"),
       port: 3001,
     },
     output: {
-      publicPath: '/',
+      publicPath: "/",
     },
     module: {
       rules: [
         {
           test: /\.jsx?$/,
-          loader: 'babel-loader',
+          loader: "babel-loader",
           exclude: /node_modules/,
           options: {
-            presets: ['@babel/preset-react'],
+            presets: ["@babel/preset-react"],
           },
         },
       ],
     },
     plugins: [
       new ModuleFederationPlugin({
-        name: 'app1',
+        name: "app1",
         remotes: {
-          app2: 'app2@[app2Url]/app2/remoteEntry.js',
-          app3: 'app3@[app2Url]/app3/remoteEntry.js',
+          app2: "app2@localhost:3001/app2/remoteEntry.js",
+          app3: "app3@localhost:3001/app3/remoteEntry.js",
         },
         shared: {
           react: { singleton: true },
-          'react-dom': { singleton: true },
+          "react-dom": { singleton: true },
         },
       }),
       new ExternalTemplateRemotesPlugin(),
       new HtmlWebpackPlugin({
-        template: './public/index.html',
+        template: "./public/index.html",
       }),
     ],
   },
   app2Config,
-  app3Config
+  app3Config,
 ];
